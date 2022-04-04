c7b1860524b4acec27cf6fdf49f26929f222ff95
Ayonam Ray <ayonam@helprack.com>
2022-04-04 23:58:47 +0530

    run clang-format

--- src/Compiler/CompilerUtils.cpp
+++ src/Compiler/CompilerUtils.cpp
@@ -48,7 +48,6 @@ using namespace onnx_mlir;
 using namespace mlir::torch;
 using namespace mlir::torch::Torch;
 
-
 llvm::cl::OptionCategory OnnxMlirOptions(
     "ONNX-MLIR Options", "These are frontend options.");
 
@@ -697,7 +696,8 @@ void registerDialects(mlir::MLIRContext &context) {
   context.getOrLoadDialect<mlir::ONNXDialect>();
   context.getOrLoadDialect<mlir::KrnlOpsDialect>();
   context.getOrLoadDialect<mlir::torch::Torch::TorchDialect>();
-  context.getOrLoadDialect<mlir::torch::TorchConversion::TorchConversionDialect>();
+  context
+      .getOrLoadDialect<mlir::torch::TorchConversion::TorchConversionDialect>();
 }
 
 void addONNXToMLIRPasses(mlir::PassManager &pm) {
@@ -733,18 +733,18 @@ void addONNXToMLIRPasses(mlir::PassManager &pm) {
     }
   }
 
-  //pm.addNestedPass<FuncOp>(mlir::createONNXToAtenLeakyReluOpTransformPass());
-  //pm.addNestedPass<FuncOp>(mlir::createONNXToAtenMaxPool2dOpTransformPass());
-  //pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConv2DOpTransformPass());
-  //pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConstantOpTransformPass());
-  //pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConstantPadNdOpTransformPass());
-  
+  // pm.addNestedPass<FuncOp>(mlir::createONNXToAtenLeakyReluOpTransformPass());
+  // pm.addNestedPass<FuncOp>(mlir::createONNXToAtenMaxPool2dOpTransformPass());
+  // pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConv2DOpTransformPass());
+  // pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConstantOpTransformPass());
+  // pm.addNestedPass<FuncOp>(mlir::createONNXToAtenConstantPadNdOpTransformPass());
+
   // Clean dead code.
   pm.addPass(mlir::createSymbolDCEPass());
 }
 
 void addONNXToTorchPasses(mlir::PassManager &pm, int optLevel) {
-  //pm.addNestedPass<FuncOp>(mlir::createONNXPreKrnlVerifyPass());
+  // pm.addNestedPass<FuncOp>(mlir::createONNXPreKrnlVerifyPass());
   // Add instrumentation for Onnx Ops
   pm.addNestedPass<ModuleOp>(mlir::createInstrumentONNXPass());
 
@@ -758,9 +758,9 @@ void addONNXToTorchPasses(mlir::PassManager &pm, int optLevel) {
   // An additional pass of canonicalization is helpful because lowering
   // from ONNX dialect to Standard dialect exposes additional canonicalization
   // opportunities.
-  //pm.addPass(mlir::createCanonicalizerPass());
-  //pm.addNestedPass<FuncOp>(createDisconnectKrnlDimFromAllocPass());
-  //pm.addPass(mlir::createCanonicalizerPass());
+  // pm.addPass(mlir::createCanonicalizerPass());
+  // pm.addNestedPass<FuncOp>(createDisconnectKrnlDimFromAllocPass());
+  // pm.addPass(mlir::createCanonicalizerPass());
 }
 
 void addONNXToKrnlPasses(mlir::PassManager &pm, int optLevel) {
@@ -1059,8 +1059,8 @@ void setupModule(mlir::OwningModuleRef &module, mlir::MLIRContext &context,
   }
 
   /// torch pass has been added
-  ///if (inputIRLevel <= ONNXLevel && emissionTarget >= EmitONNXIR)
-     addONNXToTorchPasses(pm, OptimizationLevel);
+  /// if (inputIRLevel <= ONNXLevel && emissionTarget >= EmitONNXIR)
+  addONNXToTorchPasses(pm, OptimizationLevel);
 
   if (inputIRLevel <= LLVMLevel && emissionTarget >= EmitLLVMIR)
     addKrnlToLLVMPasses(pm);
