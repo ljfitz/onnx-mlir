733cf9c04b36b1c2be3ca77f8f0c89729bbbf703
Philipp Braun <philippb@xilinx.com>
2022-07-15 08:21:43 +0100

    Add unary op conversion passes

---
+++ src/Conversion/ONNXToTOSA/Math/Elementwise.cpp
@@ -0,0 +1,68 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---------------- Elementwise.cpp - Elementwise Op --------------------===//
+//
+// Copyright (c) 2022 Advanced Micro Devices, Inc.
+//
+// =============================================================================
+//
+// This file lowers ONNX element-wise operators to TOSA dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/Conversion/ONNXToTOSA/ONNXToTOSACommon.hpp"
+
+using namespace mlir;
+
+namespace onnx_mlir {
+
+namespace {
+
+template <typename ONNXOpT, typename TOSAOpT>
+class ONNXUnaryOpLoweringToTOSA : public OpConversionPattern<ONNXOpT> {
+public:
+  using OpConversionPattern<ONNXOpT>::OpConversionPattern;
+  using OpAdaptor = typename ONNXOpT::Adaptor;
+  LogicalResult matchAndRewrite(ONNXOpT op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<TOSAOpT>(op, op.getType(), adaptor.X());
+    return success();
+  }
+};
+
+class ONNXReluOpLoweringToTOSA : public OpConversionPattern<ONNXReluOp> {
+public:
+  using OpConversionPattern::OpConversionPattern;
+  LogicalResult matchAndRewrite(ONNXReluOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+
+    // Rescale the input for quantized types. TBD
+    // Maps to tosa.clamp which has both int and fp limits.
+    Value input = adaptor.X();
+
+    rewriter.replaceOpWithNewOp<tosa::ClampOp>(op, op.getType(), input,
+        rewriter.getI64IntegerAttr(0),
+        rewriter.getI64IntegerAttr(std::numeric_limits<int32_t>::max()),
+        rewriter.getF32FloatAttr(0.0f),
+        rewriter.getF32FloatAttr(std::numeric_limits<float>::max()));
+    return success();
+  }
+};
+
+} // namespace
+
+void populateLoweringONNXElementwiseOpToTOSAPattern(RewritePatternSet &patterns,
+    TypeConverter &typeConverter, MLIRContext *ctx) {
+  patterns.insert<ONNXReluOpLoweringToTOSA>(typeConverter, ctx);
+
+#define INSERT_UNARY_PATTERN(ONNXOp, TOSAOp)                                   \
+  patterns.insert<ONNXUnaryOpLoweringToTOSA<ONNXOp, TOSAOp>>(                  \
+      typeConverter, ctx);
+  INSERT_UNARY_PATTERN(ONNXNegOp, tosa::NegateOp)
+  INSERT_UNARY_PATTERN(ONNXFloorOp, tosa::FloorOp)
+#undef INSERT_UNARY_PATTERN
+}
+
+} // namespace onnx_mlir
