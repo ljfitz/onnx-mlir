92b31bab66aa4772b9a54c2ae38a3e07ccf420eb
Philip Lassen <philiplassen+git@gmail.com>
2022-10-07 08:30:46 -0700

    Topological sort implementation (#1725)
    * Topological sort implementation
    
    Co-authorded-by: jooybar <jooybar@gmail.com>
    Signed-off-by: Philip Lassen <philiplassen+git@gmail.com>
    
    * Address comments
    
    Signed-off-by: Philip Lassen <plassen@groq.com>
    
    * Improve name of constants
    
    Signed-off-by: Philip Lassen <plassen@groq.com>
    
    * clang format
    
    Signed-off-by: Philip Lassen <plassen@groq.com>
    
    Signed-off-by: Philip Lassen <philiplassen+git@gmail.com>
    Signed-off-by: Philip Lassen <plassen@groq.com>

--- src/Compiler/CompilerUtils.cpp
+++ src/Compiler/CompilerUtils.cpp
@@ -651,6 +651,7 @@ int processInputFile(StringRef inputFilename, mlir::MLIRContext &context,
     options.useOnnxModelTypes = useOnnxModelTypes;
     options.invokeOnnxVersionConverter = invokeOnnxVersionConverter;
     options.shapeInformation = shapeInformation;
+    options.allowSorting = allowSorting;
     options.externalDataDir = dirName(inputFilename);
     return ImportFrontendModelFile(
         inputFilename, context, module, errorMessage, options);
@@ -666,6 +667,7 @@ int processInputArray(const void *onnxBuffer, int bufferSize,
   ImportOptions options;
   options.useOnnxModelTypes = useOnnxModelTypes;
   options.invokeOnnxVersionConverter = invokeOnnxVersionConverter;
+  options.allowSorting = allowSorting;
   options.shapeInformation = shapeInformation;
   return ImportFrontendModelArray(
       onnxBuffer, bufferSize, context, module, errorMessage, options);
